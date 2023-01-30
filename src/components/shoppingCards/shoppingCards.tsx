0c6c1086faff4951f259e8a6ea021e23832a379e
Anurag Prajapati <anuragyuvasoft386@gmail.com>
2023-01-30 11:21:08 +0530

    add a shpping page

---
+++ src/components/shoppingCards/shoppingCards.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+
+const ShoppingCards = () => {
+  return (
+    <div>
+        <Container>
+            <Row>
+                <Col>
+                     
+                </Col>
+            </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default ShoppingCards;
---
+++ src/components/shoppingCards/shoppingCardsData.ts
@@ -0,0 +1,22 @@
+import handfreeImg1 from '../../assets/shoppingCards/handfreeImg1.jpg';
+import handfreeImg2 from '../../assets/shoppingCards/handfreeImg2.jpg';
+import handfreeImg3 from '../../assets/shoppingCards/handfreeImg3.jpg';
+import handfreeImg4 from '../../assets/shoppingCards/handfreeImg4.jpg';
+import handfreeImg5 from '../../assets/shoppingCards/handfreeImg5.jpg';
+import handfreeImg6 from '../../assets/shoppingCards/handfreeImg6.jpg';
+import handfreeImg7 from '../../assets/shoppingCards/handfreeImg7.jpg';
+import handfreeImg8 from '../../assets/shoppingCards/handfreeImg8.jpg';
+import handfreeImg9 from '../../assets/shoppingCards/handfreeImg9.jpg';
+import handfreeImg14 from '../../assets/shoppingCards/handfreeImg10.jpg';
+import handfreeImg10 from '../../assets/shoppingCards/handfreeImg11.jpg';
+import handfreeImg11 from '../../assets/shoppingCards/handfreeImg12.jpg';
+import handfreeImg12 from '../../assets/shoppingCards/handfreeImg13.jpg';
+import handfreeImg13 from '../../assets/shoppingCards/handfreeImg14.jpg';
+
+export const  ShoppingCardsData = [
+     {
+        id : 0,
+        img : handfreeImg1,
+        title : "BEST HANDSET",
+     },
+]
