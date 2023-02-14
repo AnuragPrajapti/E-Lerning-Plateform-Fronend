6731f9f577a4c24b3b72c10122757cec3d437d75
Anurag Prajapati <anuragyuvasoft386@gmail.com>
2023-02-14 11:46:59 +0530

    chnage a backend url

--- src/components/shoppingCards/shoppingCards.tsx
+++ src/components/shoppingCards/shoppingCards.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
+import { ShoppingCardsData } from './shoppingCardsData';
+import './shoppingCards.scss';
+import ShoppingNavbar from './shoppingNavbar';
 
 const ShoppingCards = () => {
   return (
-    <div>
-        <Container>
-            <Row>
-                <Col>
-                     
-                </Col>
-            </Row>
-        </Container>
+    <div className='shopping-card-wrapper' >
+      <Row>
+          <Col>
+            <ShoppingNavbar />
+          </Col>
+        </Row>
+      <Container>
+        <Row>
+          <Col className='shopping-card-img' >
+            {
+              ShoppingCardsData ? ShoppingCardsData.map((value, index) => {
+                return (
+                  <>
+                    <img src={value?.img} alt="" />
+                  </>
+                )
+              }) : ""
+            }
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
--- src/components/shoppingCards/shoppingCardsData.ts
+++ src/components/shoppingCards/shoppingCardsData.ts
@@ -7,16 +7,80 @@ import handfreeImg6 from '../../assets/shoppingCards/handfreeImg6.jpg';
 import handfreeImg7 from '../../assets/shoppingCards/handfreeImg7.jpg';
 import handfreeImg8 from '../../assets/shoppingCards/handfreeImg8.jpg';
 import handfreeImg9 from '../../assets/shoppingCards/handfreeImg9.jpg';
-import handfreeImg14 from '../../assets/shoppingCards/handfreeImg10.jpg';
 import handfreeImg10 from '../../assets/shoppingCards/handfreeImg11.jpg';
 import handfreeImg11 from '../../assets/shoppingCards/handfreeImg12.jpg';
 import handfreeImg12 from '../../assets/shoppingCards/handfreeImg13.jpg';
 import handfreeImg13 from '../../assets/shoppingCards/handfreeImg14.jpg';
+import handfreeImg14 from '../../assets/shoppingCards/handfreeImg10.jpg';
 
-export const  ShoppingCardsData = [
-     {
-        id : 0,
-        img : handfreeImg1,
-        title : "BEST HANDSET",
-     },
+export const ShoppingCardsData = [
+   {
+      id: 0,
+      img: handfreeImg1,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 1,
+      img: handfreeImg2,
+      title: "WIRE LESS",
+   },
+   {
+      id: 2,
+      img: handfreeImg3,
+      title: "EMJOYE YOUR MUSIC",
+   },
+   {
+      id: 3,
+      img: handfreeImg4,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 4,
+      img: handfreeImg5,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 5,
+      img: handfreeImg6,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 6,
+      img: handfreeImg7,
+      title: "BEST HANDSET",
+   }, {
+      id: 7,
+      img: handfreeImg8,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 8,
+      img: handfreeImg9,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 9,
+      img: handfreeImg10,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 10,
+      img: handfreeImg11,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 11,
+      img: handfreeImg12,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 12,
+      img: handfreeImg13,
+      title: "BEST HANDSET",
+   },
+   {
+      id: 13,
+      img: handfreeImg14,
+      title: "BEST HANDSET",
+   },
 ]
